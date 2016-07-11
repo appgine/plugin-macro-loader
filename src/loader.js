d2f46f7edbf436664865d475d83a8e17b57a447f
Ales Kafka <ales.kafka@gmail.com>
2016-07-11 15:12:36 +0200

    Add targetId in resolveDataAttribute

--- src/loader.js
+++ src/loader.js
@@ -424,14 +424,15 @@ export function resolveDataAttribute($element, attrName, fn)
 	($element.getAttribute(attrName)||'').split('$').filter(attrValue => attrValue).map(function(attrValue) {
 		let [attrPlugin, ...pvar] = attrValue.split(':');
 
-		const matched = attrPlugin.match(/^(?:(.*)@)?(([^\[]*)(?:\[([^\]]*)\])?)$/);
+		const matched = attrPlugin.match(/^(?:([^\[]*)(?:\[([^\]]*)\])?@)?(([^\[]*)(?:\[([^\]]*)\])?)$/);
 		const target = matched[1]||'';
-		const name = matched[2] + (matched[4]===undefined ? "[]" : "");
-		const pluginName = matched[3];
-		const pluginId = matched[4]||'';
+		const targetId = matched[2]||'';
+		const name = matched[3] + (matched[5]===undefined ? "[]" : "");
+		const pluginName = matched[4];
+		const pluginId = matched[5]||'';
 		const data = loadData(pvar.join(':') || null)
 
-		fn({ pluginName, pluginId, target, name, data });
+		fn({ pluginName, pluginId, target, targetId, name, data });
 	});
 }
 
