d371f195e1537b39b09ed782e6a67e717b438b4b
Ales Kafka <ales.kafka@gmail.com>
2016-07-12 14:18:23 +0200

    Add bindApi hotReload support

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 
+import { findPlugins, pluginHotReload } from './loader'
 
 export default PluginApi;
+
+const apiBinder = {};
 
 
 /**
@@ -8,26 +11,79 @@ export default PluginApi;
  * @param {function|object}
  */
 export function bindApi(name, api) {
-	api = api.default || api;
+	apiBinder[name] = apiBinder[name] || createBinder(name);
+	apiBinder[name].hotReload(api);
+	return apiBinder[name];
+}
 
-	if (typeof api === 'function') {
-		api = {[name]: api};
-	}
 
-	Object.keys(api).forEach(key => {
-		if (key==='destroy') {
-			PluginApi._destroy[name] = api[key];
+function createBinder(name) {
+	let hotReloaded = false;
+	let prevApi = null;
 
-		} else if (PluginApi.prototype[key]!==undefined) {
-			throw new Error("PluginApi method '" + key + "' already exists");
+	return {
+		hotReload(api) {
+			api = api.default || api;
 
-		} else {
-			PluginApi.prototype[key] = function() {
-				const apiFn = api[key].default || api[key];
-				return this._context[name] = apiFn(this._context[name], ...arguments);
-			};
+			if (typeof api === 'function') {
+				api = {[name]: api};
+			}
+
+			if (prevApi) {
+				Object.keys(prevApi).forEach(key => {
+					if (key!=='destroy') {
+						delete PluginApi.prototype[key];
+					}
+				});
+			}
+
+			const exists = [];
+			Object.keys(api).forEach(key => {
+				if (key!=='destroy' && PluginApi.prototype[key]!==undefined) {
+					exists.push(key);
+				}
+			});
+
+			if (exists.length) {
+				throw new Error("PluginApi methods '" + exists.join(', ') + "' already exist.");
+			}
+
+			Object.keys(api).forEach(key => {
+				if (key!=='destroy') {
+					PluginApi.prototype[key] = function() {
+						const apiFn = api[key].default || api[key];
+						return this._context[name] = apiFn(this._context[name], ...arguments);
+					};
+				}
+			});
+
+			hotReloaded = true;
+			prevApi = api;
+
+			let error = undefined;
+			findPlugins(({ pluginApi }) => (name in pluginApi._context)).
+				forEach(pluginObj => {
+					try {
+						pluginHotReload(pluginObj, pluginObj.plugin);
+
+					} catch (e) {
+						error = e;
+					}
+				});
+
+			PluginApi._destroy[name] = api.destroy;
+			if (error) throw error;
+		},
+		willDispose() {
+			hotReloaded = false;
+
+			setTimeout(() => {
+				if (hotReloaded===false) {
+					this.hotReload({});
+				}
+			}, 0);
 		}
-	});
+	}
 }
 
 
