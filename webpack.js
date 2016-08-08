d02eb7c94ecf9e5e937600cd001df2e11b61b4ee
Ales Kafka <ales.kafka@gmail.com>
2016-08-08 09:39:22 +0200

    Add backward compatibility for webpack

--- webpack.js
+++ webpack.js
@@ -1,1 +1,5 @@
-module.exports = require('./lib/webpack');
+var webpack = require('./lib/webpack');
+module.exports = webpack.default;
+module.exports.default = webpack.default;
+module.exports.loader = webpack.loader;
+module.exports.loaderGlobal = webpack.loaderGlobal;
