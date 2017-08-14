44d5e56490db32f406699f2c6bbd26b8fae3f0b3
Ales Kafka <ales.kafka@gmail.com>
2017-08-14 17:18:42 +0200

    Change isArgumentObj to detect plugin variable name

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ export function isArgumentObj(fn) {
 	if (fn.toString().match(/^[^\(]*\(\s*\{/)) {
 		return true;
 
-	} else if (fn.toString().match(/^[^\(]*\(\s*_ref\s*\)/)) {
+	} else if (fn.toString().match(/^[^\(]*\(\s*(_ref|plugin)\s*\)/)) {
 		return true;
 	}
 
