a2e89aeedf571befe57277f2d80144300453243f
Ales Kafka <aleskafka@Ales-MacBook-Pro-2.local>
2020-02-04 11:15:08 +0100

    Add script[data-plugin] with data in textContent

--- src/lib/resolveDataAttribute.js
+++ src/lib/resolveDataAttribute.js
@@ -8,6 +8,15 @@ import { loadData } from '../loader/scripts'
  */
 export default function resolveDataAttribute($element, attrName, fn)
 {
+	function pluginLoadData(pvar) {
+		if (String($element.tagName||'').toLowerCase()==='script' && pvar===null) {
+			try { return JSON.parse($element.textContent); } catch (e) {}
+			return null;
+		}
+
+		return loadData(pvar);
+	}
+
 	($element.getAttribute(attrName)||'').split('$').filter(attrValue => attrValue).map(function(attrValue) {
 		let [attrPlugin, ...pvar] = attrValue.split(':');
 
@@ -17,7 +26,7 @@ export default function resolveDataAttribute($element, attrName, fn)
 		const name = matched[3] + (matched[5]===undefined ? "[]" : "");
 		const pluginName = matched[4];
 		const pluginId = matched[5]||'';
-		const createData = () => loadData(pvar.join(':') || null)
+		const createData = () => pluginLoadData(attrValue.indexOf(':')===-1 ? null : pvar.join(':'))
 		const data = createData();
 
 		fn({ pluginName, pluginId, target, targetId, name, data, createData });
